ff35e7df332d796bd38ea8d27197e61211e3c354
Tay Jun Jie <jjat1987@gmail.com>
2020-02-08 21:08:02 +0800

    Correct mktemp command

--- download_pypi.sh
+++ download_pypi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define variables
-DIR_DOWNLOADS=mktemp
+DIR_DOWNLOADS=$(mktemp -d)
 DIR_MIRROR="simple/"
 
 # Setup conda
@@ -20,26 +20,26 @@ fi
 # Download python packages
 while read PKG
 do
-    BINARY=0
-    for PLATFORM in win_amd64 manylinux1_x86_64 linux_x86_64 manylinux2014_x86_64 manylinux2010_x86_64 any
+  BINARY=0
+  for PLATFORM in win_amd64 manylinux1_x86_64 linux_x86_64 manylinux2014_x86_64 manylinux2010_x86_64 any
+  do
+    for ABI in none abi3 cp37m
     do
-       for ABI in none abi3 cp37m
-       do
-        for IMPL in py cp
+      for IMPL in py cp
+      do
+        for VER in 37 36 35 34 33 32 3 30
         do
-            for VER in 37 36 35 34 33 32 3 30
-            do
-                pypi-mirror download \
-                    -d $DIR_DOWNLOADS \
-                    --platform $PLATFORM \
-                    --abi $ABI \
-                    --implementation $IMPL \
-                    --python-version $VER \
-                    $PKG && BINARY=1
-            done
+          pypi-mirror download \
+            -d $DIR_DOWNLOADS \
+            --platform $PLATFORM \
+            --abi $ABI \
+            --implementation $IMPL \
+            --python-version $VER \
+            $PKG && BINARY=1
         done
-       done
+      done
     done
+  done
     if [ $BINARY-eq 0 ]
     then
         pypi-mirror download -d $DIR_DOWNLOADS $pkg
